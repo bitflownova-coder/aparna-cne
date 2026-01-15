e5abcc27f0c41ca9dde64128c5ddb4f0c9b73673
Bitflow Software <bit.flow.nova.gmap@gmail.com>
2026-01-15 12:25:09 +0530

    Fix database credentials in recalculate script

--- recalculate-counts.js
+++ recalculate-counts.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2/promise');
 async function recalculate() {
   const pool = await mysql.createPool({
     host: '127.0.0.1',
-    user: 'u984810592_aparna_cne',
-    password: 'Aparna@123456',
+    user: 'u984810592_aparna_admin',
+    password: 'sCARFACE@2003?.',
     database: 'u984810592_aparna_cne'
   });
   
